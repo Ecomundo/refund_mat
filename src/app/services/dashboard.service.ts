9041e859d259294295385a8ff7fe881d67b29b02
LuisAlavaWM <lalava@ecomundo.edu.ec>
2019-11-07 14:20:03 -0500

    Modificaciones el servicio refund()

--- src/app/services/dashboard.service.ts
+++ src/app/services/dashboard.service.ts
@@ -20,10 +20,10 @@ export class DashboardService {
     console.log(typeof refund);
     console.log(refund);
 
-    const json = JSON.stringify(refund);
-    console.log(typeof json);
-    console.log(json);
-    const params = json;
+    /*const json = JSON.stringify(refund);
+    console.log(typeof json);*/
+    console.log(refund.transaction_id);
+    const params = refund.transaction_id;
     const headers = new HttpHeaders({'Content-Type': 'application/json','Authorization': 'bearer ' + localStorage.getItem('token')});
 
     return this._http.post(this.url + 'refundDebtBase', params, { headers });
